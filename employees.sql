ea15d791b5e619fef38eb6155a25a57515ac62ea
Lusindiso <lusindiso@projectcodex.co>
2016-05-05 10:42:14 +0200

    initial commit

---
+++ employees.sql
@@ -0,0 +1,4 @@
+INSERT INTO employees (name, employees_no) VALUES('Jesse', 149);
+INSERT INTO employees (name, employees_no) VALUES('Lwazi', 143);
+INSERT INTO employees (name, employees_no) VALUES('Tom', 154);
+INSERT INTO employees (name, employees_no) VALUES('katty', 144);
